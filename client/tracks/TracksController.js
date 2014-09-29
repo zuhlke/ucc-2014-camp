ace2dd40cddd26d1281b4e5664f8f6a44ed7834f
Wojciech Słowacki <wojciech.slowacki@gmail.com>
2014-09-29 17:41:57 +0200

    removing 'controller as' syntax from tracks

--- client/tracks/TracksController.js
+++ client/tracks/TracksController.js
@@ -1,25 +1,24 @@
-myapp.controller('TracksController', function() {
-    var _root = this;
+myapp.controller('TracksController', function($scope) {
 
     function addTrack (name, author) {
         var newTrack = {'name' : name, 'author' : author};
-        _root.list.push(newTrack);
+        $scope.list.push(newTrack);
     }
 
     function removeTrack (position) {
-        _root.list.splice(position,1);
+        $scope.list.splice(position,1);
     }
 
-    this.list = [
+    $scope.list = [
         {'name':'Song1', 'currentlyPlaying' : true },
         {'name':'Song2' },
         {'name':'Song3' },
         {'name':'Song4' }
     ];
 
-    this.addNewTrack = addTrack;
-    this.removeTrack = removeTrack;
-    this.fileDropped = function (file) {
-        _root.list.push({name: file.name})
+    $scope.addNewTrack = addTrack;
+    $scope.removeTrack = removeTrack;
+    $scope.fileDropped = function (file) {
+        $scope.list.push({name: file.name})
     }
 });
