fce237a3a40d05abaec98ba48aa2adfa39dec047
fibinger <jfibinger@gmail.com>
2014-09-29 12:29:13 +0200

    Source connected to destination and added gain node

--- public/audio-example/AudioExample.js
+++ public/audio-example/AudioExample.js
@@ -7,3 +7,11 @@ var audioElement = document.querySelector('audio');
 var audioContext = new AudioContext();
 var source = audioContext.createMediaElementSource(audioElement);
 
+var gainNode = audioContext.createGain();
+gainNode.gain.value = 0.5;
+source.connect(gainNode);
+
+gainNode.connect(audioContext.destination);
+
+
+//source.start(0);
