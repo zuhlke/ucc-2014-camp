4678c8b873a3ec88973baa9a0163aed7726a988a
Wojciech Słowacki <wojciech.slowacki@gmail.com>
2014-09-29 17:22:53 +0200

    add/remove handlers for tracks and playlists lists

--- client/playlists/PlaylistsController.js
+++ client/playlists/PlaylistsController.js
@@ -1,6 +1,20 @@
 myapp.controller('PlaylistsController', function() {
+    var _root = this;
+
     this.list = [
         {'name':'My playlist1' },
         {'name':'My playlist2' }
     ];
+
+    function addPlaylist (name) {
+        var newPlaylist = {'name' : name};
+        _root.list.push(newPlaylist);
+    }
+
+    function removePlaylist (position) {
+        _root.list.splice(position,1);
+    }
+
+    this.addNewPlaylist = addPlaylist;
+    this.removePlaylist = removePlaylist;
 });
--- client/tracks/TracksController.js
+++ client/tracks/TracksController.js
@@ -1,8 +1,22 @@
 myapp.controller('TracksController', function() {
+    var _root = this;
+
+    function addTrack (name, author) {
+        var newTrack = {'name' : name, 'author' : author};
+        _root.list.push(newTrack);
+    }
+
+    function removeTrack (position) {
+        _root.list.splice(position,1);
+    }
+
     this.list = [
         {'name':'Song1', 'currentlyPlaying' : true },
         {'name':'Song2' },
         {'name':'Song3' },
         {'name':'Song4' }
     ];
+
+    this.addNewTrack = addTrack;
+    this.removeTrack = removeTrack;
 });
