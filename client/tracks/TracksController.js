87a774a4916c94bd0d3f91ebf4b0ead69107ed2a
Kevin <kevin.denver@gmail.com>
2014-09-29 18:01:11 +0200

    Clicking on X removes an item from the track list.

--- client/tracks/TracksController.js
+++ client/tracks/TracksController.js
@@ -15,7 +15,13 @@ myapp.controller('TracksController', function ($scope, $rootScope) {
         $rootScope.$broadcast('startPlaying', e.target.result);
       };
     })(track.file);
-
     fileReader.readAsDataURL(track.file);
   };
+
+  $scope.removeTrack = function(track) {
+    _($scope.tracks).remove(function(t) {
+      return t === track;
+    });
+  };
+
 });
