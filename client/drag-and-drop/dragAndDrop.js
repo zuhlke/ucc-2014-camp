aabba394e55b5787d97e615e81485829f3f413c8
heldstab <christian.heldstab@gmail.com>
2014-09-30 09:45:27 +0200

    style drop-zone

--- client/drag-and-drop/dragAndDrop.js
+++ client/drag-and-drop/dragAndDrop.js
@@ -1,41 +1,51 @@
 myapp.directive('dragAndDrop', function ($rootScope) {
 
-  return {
-    restrict: 'A',
-    link: function (scope, element, attrs) {
+    return {
+        restrict: 'A',
+        link: function (scope, element, attrs) {
 
-      element.attr("draggable", true);
+            var dragDropClass = 'drop-zone';
+            var dragDropActiveClass = 'drop-zone_active';
+            element.addClass(dragDropClass);
 
-      element.bind('dragstart', function(e) {
-        console.log(e.type);
-      });
+            element.attr("draggable", true);
 
-      element.bind('dragend', function(e) {
-        console.log(e.type);
-      });
+            element.bind('dragstart', function (e) {
+                element.addClass(dragDropActiveClass);
+                console.log(e.type);
+            });
 
-      element.bind('dragenter', function(e) {
-        console.log(e.type);
-      });
+            element.bind('dragend', function (e) {
+                element.removeClass(dragDropActiveClass);
+                console.log(e.type);
+            });
 
-      element.bind('dragover', function(e) {
-        console.log(e.type);
-        e.preventDefault();
-      });
+            element.bind('dragenter', function (e) {
+                element.addClass(dragDropActiveClass);
+                console.log(e.type);
+            });
 
-      element.bind('dragleave', function(e) {
-        console.log(e.type);
-      });
+            element.bind('dragover', function (e) {
+                console.log(e.type);
+                e.preventDefault();
+            });
 
-      element.bind('drop', function(e) {
-        e.preventDefault();
-        var file = e.originalEvent.dataTransfer.files[0];
-        scope.$apply(function() {
-          scope.$emit('fileDropped', file);
-        });
-        return false;
-      });
+            element.bind('dragleave', function (e) {
+                element.removeClass(dragDropActiveClass);
+                console.log(e.type);
+            });
 
-    }
-  };
+            element.bind('drop', function (e) {
+                element.removeClass(dragDropActiveClass);
+
+                e.preventDefault();
+                var file = e.originalEvent.dataTransfer.files[0];
+                scope.$apply(function () {
+                    scope.$emit('fileDropped', file);
+                });
+                return false;
+            });
+
+        }
+    };
 });
