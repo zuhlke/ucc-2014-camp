30573ac21ab670e4a1c15774f4ca9f8b467d5511
Kevin <kevin.denver@gmail.com>
2014-10-02 15:38:26 +0200

    The AudioService got too complex. Split up into smaller objects and services.

---
+++ client/audio/AudioPlayer.js
@@ -0,0 +1,67 @@
+myapp.factory('AudioPlayer', function ($rootScope, $q, $window) {
+
+  $window.AudioContext = $window.AudioContext || $window.webkitAudioContext;
+
+  var context = new AudioContext();
+
+  var AudioPlayer = function(track) {
+    this.context = context;
+    this.track = track;
+    this._trackBuffer = this._load(track);
+    this._gainNode = context.createGain();
+    this._isPlaying = false;
+  };
+
+  AudioPlayer.prototype._load = function(track) {
+    var deferred = $q.defer();
+    var fileReader = new FileReader();
+    fileReader.onload = function (e) {
+      context.decodeAudioData(e.target.result, function (buffer) {
+        deferred.resolve(buffer);
+      });
+    };
+    fileReader.readAsArrayBuffer(track.file);
+    return deferred.promise;
+  };
+
+  AudioPlayer.prototype.play = function() {
+    // Generally, javascript callbacks, change the value of the "this" variable inside it
+    // so we need to keep a reference to the current instance "this".
+    var self = this;
+
+    this._trackBuffer.then(function(buffer) {
+      self._source = context.createBufferSource();
+      self._remote =  context.createMediaStreamDestination();
+      self._source.buffer = buffer;
+      self._source.connect(context.destination);
+      self._gainNode.connect(context.destination);
+      self._source.connect(self._gainNode);
+      self._source.connect(self._remote);
+      self._source.start(0);
+
+      self._isPlaying = true;
+
+      $rootScope.$broadcast('AudioPlayer.isPlaying', {
+        isPlaying: self._isPlaying,
+        trackName: self.track.name,
+        stream: self._remote.stream
+      });
+    });
+
+  };
+
+  AudioPlayer.prototype.stop = function() {
+    this._source && this._source.stop();
+    this._isPlaying = false;
+    $rootScope.$broadcast('AudioPlayer.isPlaying', {
+      isPlaying: this._isPlaying
+    });
+  };
+
+  AudioPlayer.prototype.setVolume = function(value) {
+    this._gainNode.gain.value = value;
+  };
+
+  return AudioPlayer;
+
+});
