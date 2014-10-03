51d11d714b19e6ff12747afce7bccd441c0f4a3c
heldstab <christian.heldstab@gmail.com>
2014-10-03 15:06:19 +0200

    stop local audio when start to stream from friend

--- client/friends/FriendsController.js
+++ client/friends/FriendsController.js
@@ -1,9 +1,11 @@
-myapp.controller('FriendsController', function(webRTCService, $log) {
+myapp.controller('FriendsController', function(webRTCService, $log, audioPlayer) {
     var _root = this;
     this.list = webRTCService.getPeers();
+    this.audioPlayer = audioPlayer;
 
-    _root.openStream = function(destination) {
-      $log.debug('clicked on ' + destination);
+
+  _root.openStream = function(destination) {
+      this.audioPlayer.stop();
       webRTCService.requestStream(destination);
     }
 });
