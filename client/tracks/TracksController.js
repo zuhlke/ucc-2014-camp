d1e3b4543a7f922c8283022636b0b92bdaa4a115
Howard Perrin <howard@perrin.co.uk>
2014-10-03 15:49:49 +0200

    Load ID3 image if possible

--- client/tracks/TracksController.js
+++ client/tracks/TracksController.js
@@ -1,6 +1,34 @@
 myapp.controller('TracksController', function ($scope, $rootScope, $log, audioService) {
     $scope.tracks = [];
 
+    // public method for encoding an Uint8Array to base64
+    function encode (input) {
+        var keyStr = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=";
+        var output = "";
+        var chr1, chr2, chr3, enc1, enc2, enc3, enc4;
+        var i = 0;
+
+        while (i < input.length) {
+            chr1 = input[i++];
+            chr2 = i < input.length ? input[i++] : Number.NaN; // Not sure if the index
+            chr3 = i < input.length ? input[i++] : Number.NaN; // checks are needed here
+
+            enc1 = chr1 >> 2;
+            enc2 = ((chr1 & 3) << 4) | (chr2 >> 4);
+            enc3 = ((chr2 & 15) << 2) | (chr3 >> 6);
+            enc4 = chr3 & 63;
+
+            if (isNaN(chr2)) {
+                enc3 = enc4 = 64;
+            } else if (isNaN(chr3)) {
+                enc4 = 64;
+            }
+            output += keyStr.charAt(enc1) + keyStr.charAt(enc2) +
+                keyStr.charAt(enc3) + keyStr.charAt(enc4);
+        }
+        return output;
+    }
+
     $scope.$on('fileDropped', function (event, files) {
         _(files).forEach(function (file) {
             id3(file, function (err, tags) {
@@ -9,10 +37,16 @@ myapp.controller('TracksController', function ($scope, $rootScope, $log, audioSe
                 if (tags.artist && tags.album && tags.v1.track && tags.title) {
                     trackname = tags.artist + " - " + tags.album + " - " + tags.v1.track + ". " + tags.title;
                 }
+                var imageSource = "";
+                if (tags.v2.image.data) {
+                    var bytes = new Uint8Array(tags.v2.image.data);
+                    imageSource = "data:" + tags.v2.image.mime + ";base64," + encode(bytes);
+                }
                 $scope.$apply(function () {
                     $scope.tracks.push({
                         file: file,
                         name: trackname,
+                        imageSource: imageSource,
                         isPlaying: false,
                         isSelected: false
                     });
